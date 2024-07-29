ef923d74344eda382e3902a6ab4355c56694314f
Daniil <daniilminin1990@yandex.ru>
2024-07-29 05:12:01 +0500

    remove some unnecessary data

--- src/features/googleButton/ui/GoogleAuthButton.tsx
+++ src/features/googleButton/ui/GoogleAuthButton.tsx
@@ -2,14 +2,11 @@
 import React from 'react';
 
 import Google from '@/../public/assets/components/GoogleSvgrepoCom';
-import { useAuthGoogle } from '@/features';
 
 export const GoogleAuthButton = () => {
-  const { login } = useAuthGoogle();
-
   return (
     <>
-      <button onClick={() => login()}>
+      <button>
         <Google height={36} width={36} />
       </button>
     </>
